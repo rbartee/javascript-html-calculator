5edd4e53151430d88dcbd4886a0ac68f246f9a4e
Ragahleak Bartee <rbartee@wlapcs.org>
2017-02-22 05:39:29 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("javascript runs this code when the page loads");
+function clearDisplay(){
+    document.getElementById("display-bar").innerHTML = "";
+}
+
+
+function display(userInput){
+    document.getElementById("display-bar").innerHTML +=userInput;
+}
+
+
+function calculate(){
+   var answer = eval(document.getElementById("display-bar").innerHTML);
+ document.getElementById("display-bar").innerHTML = answer;
+}
